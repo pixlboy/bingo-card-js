f52ecbefa1a96efa0cf8dc414931bd1a65445bee
Rachit <rachigarg@gmail.com>
2020-06-19 20:11:15 +0530

    paths fixed

--- src/scripts/register-sw.js
+++ src/scripts/register-sw.js
@@ -1,7 +1,7 @@
 // Register the SW file
 
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('../sw.js')
       .then(reg => console.log('Service worker registered'))
       .catch(err => console.log('Service worker not registered ==> \n', err));
 } else {
